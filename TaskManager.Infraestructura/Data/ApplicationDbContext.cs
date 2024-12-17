ce5195d50b353cb5fea9b8eea4f78eb655122a57
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2024-12-17 15:12:34 +0100

    Project Initialization

---
+++ TaskManager.Infraestructura/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Infraestructura.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
